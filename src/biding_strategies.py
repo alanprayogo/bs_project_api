58b8e5422d1c3010b7766d4bf5bfda48fc6f9308
alanprayogo <alanbp98@gmail.com>
2025-07-07 07:16:49 +0700

    Add precision response bidding strategy for 1H

--- src/biding_strategies.py
+++ src/biding_strategies.py
@@ -3,12 +3,14 @@
 from src.prec.opening import prec_opening
 from src.prec.respon_1c import prec_respon_1c
 from src.prec.respon_1d import prec_respon_1d
+from src.prec.respon_1h import prec_respon_1h
 
 BIDING_STRATEGIES = {
     # Precision
     "prec_opening": prec_opening,
     "prec_respon_1c": prec_respon_1c,
     "prec_respon_1d": prec_respon_1d,
+    "prec_respon_1h": prec_respon_1h,
     # SAYC
     # "sayc_opening": sayc_opening,
     # "sayc_respon_1c": sayc_respon_1c,
