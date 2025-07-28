a601920c7bb6b6e5a92176538a074c72a5383ae6
alanprayogo <alanbp98@gmail.com>
2025-07-29 03:42:54 +0700

    update .gitignore after tunning extraction for nsga2

--- config_example.py
+++ config_example.py
@@ -1,4 +1,13 @@
 # config_example
 
+# local development
 HOST = "127.0.0.1"
 PORT = 8000
+
+# emulator development (sesuaikan dengan config device anda)
+# HOST = ""
+# PORT =
+
+# device development (sesuaikan dengan config device anda)
+# HOST = ""
+# PORT =
